c5036a006d7d8e934cd2fc31cde2bbd765639492
tmetcalfe89 <tmetcalfe89@gmail.com>
2023-08-27 13:37:34 -0700

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <div
+      style={{
+        height: "100vh",
+        width: "100vw",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+      }}
+    >
+      <img src="/robot_arm.svg" />
+      <p style={{ fontWeight: "bold", fontSize: "5rem" }}>Coming soon...</p>
+    </div>
+  );
+}
+
+export default App;
