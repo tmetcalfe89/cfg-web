1f5b8db1478f99b1434dc9f9171e5489833bd4e7
tmetcalfe89 <tmetcalfe89@gmail.com>
2023-08-28 23:16:28 -0700

    created basic page layout fix #3

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,54 @@
+import { ActionIcon, Button, Group, MantineProvider } from "@mantine/core";
+import useThemeDetector from "react-theme-hook";
+import { Icon123 } from "@tabler/icons-react";
+import Shell from "./components/Shell";
+import Logo from "./assets/robot_arm.svg";
+
 function App() {
+  const isDarkMode = useThemeDetector();
   return (
-    <div
-      style={{
-        height: "100vh",
-        width: "100vw",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        flexDirection: "column",
-      }}
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{ colorScheme: isDarkMode ? "dark" : "light" }}
     >
-      <img src="/cfg-web/robot_arm.svg" />
-      <p style={{ fontWeight: "bold", fontSize: "5rem" }}>Coming soon</p>
-    </div>
+      <Shell
+        pageNav={
+          <>
+            <Button>Button A</Button>
+            <Button>Button A</Button>
+            <Button>Button A</Button>
+          </>
+        }
+        siteNav={
+          <Group position="apart" sx={{ flexGrow: 1 }}>
+            <Group>
+              <Button>Button A</Button>
+              <Button>Button A</Button>
+              <ActionIcon>
+                <Icon123 />
+              </ActionIcon>
+            </Group>
+            <Group>
+              <Button>Button A</Button>
+            </Group>
+          </Group>
+        }
+      >
+        <div
+          style={{
+            height: "100%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            flexDirection: "column",
+          }}
+        >
+          <img src={Logo} />
+          <p style={{ fontWeight: "bold", fontSize: "5rem" }}>Coming soon</p>
+        </div>
+      </Shell>
+    </MantineProvider>
   );
 }
 
